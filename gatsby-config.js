a35e26d8c70ec1943a3d20ef98fa5fced21b6f6d
Denis Augsburger <denis.augsburger@twteam.ch>
2020-01-08 17:17:28 +0100

    Init starter project

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter with theme usage`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@daugsbi`,
+  },
+  plugins: [
+    `@daugsbi/gatsby-theme-core`,
+
+    `@daugsbi/gatsby-theme-seo`,
+
+    `@daugsbi/gatsby-theme-pwa`,
+  ],
+}
